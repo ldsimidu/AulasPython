773fbaccd539e5cfd358f614280076252bdead0d
Lucas Simidu <lucas.simidu@outlook.com>
2024-03-21 11:19:28 -0300

    Att

---
+++ Aula 4 - Python/ExMulta/exMultaV2.py
@@ -0,0 +1,20 @@
+v = int(input('Insira a velocidade do carro: '))
+multa = ''
+if v <= 100:
+    multa = 0
+    print('Isento')
+if v <= 120:
+    multa = (20/100)*v
+
+elif v <= 150:
+    multa = (20/100)*120 + (30/100)*v
+else:
+    multa = (20/100)*120 + (30/100)*150 + (40/100)*v
+    
+print(multa)
+
+
+#ATÉ 100 - isento
+#ATÉ 120 - 20% da v
+#ATÉ 150 - 20% 120 + 30% da v
+# > 150 - 20% 120 + 30% 150 + 40% da v

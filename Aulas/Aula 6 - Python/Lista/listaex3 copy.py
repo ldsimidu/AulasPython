05f37955485d987c1fec2dfdbea2e20b37aa7a58
Lucas Simidu <lucas.simidu@outlook.com>
2024-05-02 11:41:07 -0300

    att

---
+++ Aulas/Aula 6 - Python/Lista/listaex3 copy.py
@@ -0,0 +1,7 @@
+profsA = ['Andre','Lucas','Yan','Danilo','Allen','Fabio','Celso']
+profsB = ['Lucas','Buceta','Pau','Viado Broxa', 'Danilo']
+
+for profA in profsA:
+    for profB in profsB:
+        if profA == profB:
+            print(f'O {profA} da aula nas duas turmas')
